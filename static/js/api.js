3f5a4c371ac04afb795b634464699d0c79a9a3c6
Blaon <iloveelevation@gmail.com>
2020-02-13 12:30:02 +0100

    start 0.4

--- static/js/api.js
+++ static/js/api.js
@@ -1,10 +1,9 @@
-fetch('https://swapi.co/api/planets/', {
-    method: 'POST'
-})
+fetch('https://swapi.co/api/planets/')
 .then((response) => response.json())
 .then((data)=> {
-        header();
-        // process(data);
+    header();
+    process(data);
+    console.log(data);
 
 });
 
@@ -14,21 +13,40 @@ function header() {
         let header_name = document.createElement("div");
         header_name.textContent = header;
         header_name.style.fontWeight = 'bold';
+        header_name.id = 'stable_div';
         document.getElementById('stable').appendChild(header_name)
     }
 }
 
 function process(data) {
-    let keys = ['name', 'diameter', 'climate', 'terrain', 'surface_water', 'population', 'residents.length', 'vote'];
+    let keys = ['name', 'diameter', 'climate', 'terrain', 'surface_water', 'population', 'residents', 'vote'];
     for (let planet of data.results) {
-        for (let ertec of keys){
-            if (ertec !== 'vote'){
+        for (let key of keys){
+            if (key !== 'vote' && key !== 'residents'){
                 let new_row = document.createElement('div');
-                new_row.textContent = data[ertec];
+                new_row.textContent = planet[key];
+                new_row.id = 'stable_div';
                 document.getElementById('stable').appendChild(new_row)
+            }
+            else if (key === 'residents'){
+                if (planet[key].length < 1){
+                    let resident = document.createElement('div');
+                    resident.textContent = 'no known residents';
+                    resident.id = 'stable_div';
+                    document.getElementById('stable').appendChild(resident)
+                }
+                else{
+                    let resident = document.createElement('button');
+                    resident.textContent = `${planet[key].length} resident(s)`;
+                    resident.id = 'stable_buttons';
+                    document.getElementById('stable').appendChild(resident)
+                }
             }
             else{
                 let vote_button = document.createElement('button');
+                vote_button.textContent = 'VOTE';
+                vote_button.id = 'stable_buttons';
+                document.getElementById('stable').appendChild(vote_button)
 
             }
         }
